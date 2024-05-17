3980772264a9c7ca69bed4b9256a7a49ebda2b5c
Wyllas <93063485+WyllasSidjeno@users.noreply.github.com>
2024-05-17 18:06:16 -0400

    End game trigger

--- Assets/SewerTrigger.cs
+++ Assets/SewerTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SewerTrigger : MonoBehaviour
 {
@@ -10,9 +11,11 @@ public class SewerTrigger : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(EnemyContainer.transform.childCount > 0)
+        if(EnemyContainer.transform.childCount <= 0)
         {
             Debug.Log("Sewer trigger was called and is valid.");
+            // Change scene
+            SceneManager.LoadScene("End menu");
         }
         else
         {
