919f0642643317053946219ded006d24e6406cf7
Wyllas <93063485+WyllasSidjeno@users.noreply.github.com>
2024-05-20 12:07:06 -0400

    End game transition

--- Assets/OnGameStartUtilitary.cs
+++ Assets/OnGameStartUtilitary.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(UltEventHolder))]
 public class GameHandler : MonoBehaviour {
-
-    UltEventHolder ult;
-    public void Start() {
-        ult = GetComponent<UltEventHolder>();
-        ult.Invoke();
-        
-        
-    }
     public void OnValidEndGameTrigger() {
         Debug.Log("End game trigger was called and is valid.");
         StartCoroutine(ChangeSceneAfterDelay(4f));
--- Assets/Script/Menu/MenuUtilitary.cs
+++ Assets/Script/Menu/MenuUtilitary.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UltEvents;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(UltEventHolder))]
 public class MenuUtilitary : MonoBehaviour
 {
+    UltEventHolder ult;
+
+    void Start() {
+        // if the current scene is game or end menu
+        if (SceneManager.GetActiveScene().name == "Game" || SceneManager.GetActiveScene().name == "End Menu") {
+            ult = GetComponent<UltEventHolder>();
+            ult.Invoke();
+        }
+        
+
+    }
     public void OnNewGame() {
         // Todo : Delegate and event and shit for the on black
         SceneManager.LoadScene("Game");
